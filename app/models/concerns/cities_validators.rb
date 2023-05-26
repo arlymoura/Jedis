f7bf96f69c4b9e1a6ed32d65458fadb38f91e95a
Arly Moura <arlymoura@gmail.com>
2023-05-26 13:21:49 -0300

    Add validators model city

---
+++ app/models/concerns/cities_validators.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module CitiesValidators
+  extend ActiveSupport::Concern
+
+  def cpf_is_valid?
+    return errors.add(:cpf, 'CPF invalido') unless CPF.valid?(cpf)
+  end
+
+  def email_is_valid?
+    return if /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.match?(email)
+
+    errors.add(:email,
+               'Email invalido')
+  end
+
+  def birth_date_valid?
+    return if birth_date.blank?
+
+    date_time = birth_date.to_datetime
+    year = date_time.year
+    actual_year = DateTime.now.year
+
+    return unless year > actual_year || (actual_year - year) > 120
+
+    errors.add(:birth_date,
+               'Data de nascimento invalida')
+  end
+end
